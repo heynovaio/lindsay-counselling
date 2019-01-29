c628bce6a7dd726acd27cb62b5341c4c445982c1
Bryony Anderson <bryony.a.anderson@gmail.com>
2019-01-28 17:20:50 -0800

    Set up changes only

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,16 +3,8 @@
 @section('content')
   @include('partials.page-header')
 
-  @if (!have_posts())
-    <div class="alert alert-warning">
-      {{ __('Sorry, no results were found.', 'sage') }}
-    </div>
-    {!! get_search_form(false) !!}
-  @endif
+<p>Hello World</p>
 
-  @while (have_posts()) @php the_post() @endphp
-    @include('partials.content-'.get_post_type())
-  @endwhile
+<!-- add htm here -->
 
-  {!! get_the_posts_navigation() !!}
 @endsection
